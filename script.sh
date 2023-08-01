35d6ebd9b1435c878b5b368150a28f6103631cfc
MrTalon63 <marcin@mrtalon.eu>
2023-08-01 04:02:53 +0200

    Maybe create file in a root?

--- script.sh
+++ script.sh
@@ -13,8 +13,8 @@ echo "Create PL station list"
 head -n 1 user.csv > user_PL_wo_diacritics.csv
 grep -E '[,](HF|3Z|S[NOPRQ])[0-9]([A-Z0-9]{0,3}[A-Z]{1})[,]' user_wo_diacritics.csv >> user_PL_wo_diacritics.csv
 
-mkdir -p databases
+mkdir -p /databases
 
-cp user.csv databases/
-cp user_wo_diacritics.csv databases/
-cp user_PL_wo_diacritics.csv databases/
+cp user.csv /databases/
+cp user_wo_diacritics.csv /databases/
+cp user_PL_wo_diacritics.csv /databases/
